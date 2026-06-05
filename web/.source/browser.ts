6566f24a242ad71b247645b411590fe033c68936
seqyuan <ahworld07@gmail.com>
2026-06-06 03:30:59 +0800

    docs: add comprehensive bioinformatics report example to CLI page

--- web/.source/browser.ts
+++ web/.source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"cli.mdx": () => import("../content/docs/cli.mdx?collection=docs"), "html-rendering.mdx": () => import("../content/docs/html-rendering.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "react-rendering.mdx": () => import("../content/docs/react-rendering.mdx?collection=docs"), "template-engine.mdx": () => import("../content/docs/template-engine.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"cli.mdx": () => import("../content/docs/cli.mdx?collection=docs"), "gallery.mdx": () => import("../content/docs/gallery.mdx?collection=docs"), "html-rendering.mdx": () => import("../content/docs/html-rendering.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "react-rendering.mdx": () => import("../content/docs/react-rendering.mdx?collection=docs"), "template-engine.mdx": () => import("../content/docs/template-engine.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- web/.source/server.ts
+++ web/.source/server.ts
@@ -1,8 +1,9 @@
 // @ts-nocheck
-import * as __fd_glob_5 from "../content/docs/template-engine.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/react-rendering.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/html-rendering.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/template-engine.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/react-rendering.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/html-rendering.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/gallery.mdx?collection=docs"
 import * as __fd_glob_1 from "../content/docs/cli.mdx?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
@@ -13,4 +14,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"cli.mdx": __fd_glob_1, "html-rendering.mdx": __fd_glob_2, "index.mdx": __fd_glob_3, "react-rendering.mdx": __fd_glob_4, "template-engine.mdx": __fd_glob_5, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"cli.mdx": __fd_glob_1, "gallery.mdx": __fd_glob_2, "html-rendering.mdx": __fd_glob_3, "index.mdx": __fd_glob_4, "react-rendering.mdx": __fd_glob_5, "template-engine.mdx": __fd_glob_6, });
